293ee2c899d4525049019c0d7201515905cae0c0
youssefsh <ussefo@gmail.com>
2025-03-28 18:57:21 +0200

    [SCH-3185] Added support for styled icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { IconNames } from '../types';
-import '../../dist/schematic-icons.css';
+import React, { CSSProperties, HTMLAttributes } from "react";
+import { IconNames } from "../types";
+import "../../dist/schematic-icons.css";
 
 /**
  * Props for the Icon component
  */
-interface IconProps {
+interface IconProps extends HTMLAttributes<HTMLElement> {
   /** The name of the icon to display */
   name: IconNames;
   /** Additional CSS classes to apply to the icon */
   className?: string;
   /** Additional CSS styles to apply to the icon */
-  style?: React.CSSProperties;
+  style?: CSSProperties;
 }
 
 /**
  * Icon component that displays a schematic icon
- * 
+ *
  * @example
  * ```tsx
  * <Icon name="check" />
@@ -24,14 +24,11 @@ interface IconProps {
  * <Icon name="close" style={{ color: 'red' }} />
  * ```
  */
-export const Icon = ({ name, className = '', style }: IconProps) => {
+export const Icon = ({ name, className = "", style, ...rest }: IconProps) => {
   return (
-    <i
-      className={`icon icon-${name} ${className}`}
-      style={style}
-    />
+    <i className={`icon icon-${name} ${className}`} style={style} {...rest} />
   );
 };
 
 export type { IconProps };
-export default Icon; 
+export default Icon;
