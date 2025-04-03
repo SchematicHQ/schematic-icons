80182f62aa3ba55a65988f71dc312d16d0822151
Youssefsh <Youssefsh@users.noreply.github.com>
2025-04-03 19:09:05 +0200

    Updating package name (#6)

--- release.config.mjs
+++ release.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 /** @type {import('semantic-release').GlobalConfig} */
 export default {
-  repositoryUrl: "https://github.com/schematichq/schematic-icons",
+  repositoryUrl: "https://github.com/SchematicHQ/schematic-icons.git",
   tagFormat: "v${version}",
   branches: [
     "main",
@@ -13,19 +13,12 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-      },
-    ],
-    "@semantic-release/github",
+    "@semantic-release/changelog",
     "@semantic-release/npm",
+    "@semantic-release/github",
+    ["@semantic-release/git", {
+      "assets": ["CHANGELOG.md", "package.json", "package-lock.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
   ],
 };
