fe2f929d581ace7ccb5c1ec5bea23362e8b85a17
Cole Chrzan <cole@schematichq.com>
2025-04-09 10:23:51 -0400

    chore(icon): remove comment

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,16 +5,6 @@ export interface IconProps extends React.HTMLAttributes<HTMLElement> {
   name: IconNames;
 }
 
-/**
- * Icon component that displays a schematic icon
- *
- * @example
- * ```tsx
- * <Icon name="check" />
- * <Icon name="arrow-right" className="my-icon" />
- * <Icon name="close" style={{ color: 'red' }} />
- * ```
- */
 export const Icon: React.FunctionComponent<IconProps> = ({
   name,
   className = "",
